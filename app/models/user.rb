4a354dd92cf882f8fef51eb2e43f0c0d858d4fe4
Shawn Bower <shawn.bower@gmail.com>
2011-08-25 17:52:35 -0400

    working sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,12 @@ class User < ActiveRecord::Base
 
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil if user.nil?
-    return user if user.has_password?(submitted_password)
+    return (user && user.has_password?(submitted_password)) ? user : nil
+  end
+  
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    return (user && user.salt == cookie.salt) ? user : nil
   end
 
   private 
