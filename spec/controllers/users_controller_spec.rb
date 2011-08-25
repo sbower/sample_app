1bcc4d328637838d1c74c030abd41ed4b20c647c
Shawn Bower <shawn.bower@gmail.com>
2011-08-25 00:37:46 -0400

    added gravatar

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,5 +36,14 @@ describe "GET 'show" do
       response.should have_selector("title", :content => @user.name)
     end
     
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
   end
 end
