9db1c71e3c4f157a4f94805e4578cc8f606bd0fa
Shawn Bower <shawn.bower@gmail.com>
2011-08-24 22:37:15 -0400

    stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor   :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -8,6 +9,10 @@ class User < ActiveRecord::Base
   
   validates :email, :presence => true,
                     :format   => { :with => email_regex },
-                    :uniqueness => true
+                    :uniqueness => { :case_sensitive => false }
+                    
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40}
 
 end
